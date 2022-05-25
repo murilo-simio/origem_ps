dd8b02aeceb65a3b69c916c92aa726b1715989fb
murilo-simio <murilo2110@hotmail.com>
2022-05-24 22:54:06 -0300

    Limpando o código

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@
 
 using namespace std;
 
-void printMotoInfo(string plate, float veloc, long long int batt, float carga);
+void printMotoInfo(Moto motoI);
 void printEtbInfo(int id, long long int uid, float soc, bool charg);
+Moto motoUpdate(Moto motoI, bool accel);
+
+int timer = 0;
 
 int main(){
 
@@ -16,66 +19,56 @@ int main(){
 
     Etb station = Etb();
 
-    int ciclo = 0, segundos = 0, timer = 0;
+    int ciclo = 0, segundos = 0;
 
     // Rotina 1
     for (ciclo=0;ciclo<6;ciclo++) {
         for (segundos=0;segundos<180;segundos++) {
-            moto.setSoc(moto.calculaSoc(moto.getHost(), moto.calculaVel(true)));
-            timer++;
-            if (timer%10 == 0)
-                printMotoInfo(moto.getPlate(), moto.getSpeed(), moto.getUid(), moto.getSoc());
+            moto = motoUpdate(moto, true);
         }
         for (segundos=0;segundos<10;segundos++) {
-            moto.setSoc(moto.calculaSoc(moto.getHost(), moto.calculaVel(false)));
-            timer++;
-            if (timer%10 == 0)
-                printMotoInfo(moto.getPlate(), moto.getSpeed(), moto.getUid(), moto.getSoc());
+            moto = motoUpdate(moto, false);
         }
     }
 
     // Rotina 2
     for (ciclo=0;ciclo<4;ciclo++) {
         for (segundos=0;segundos<120;segundos++) {
-            moto.setSoc(moto.calculaSoc(moto.getHost(), moto.calculaVel(true)));
-            timer++;
-            if (timer%10 == 0)
-                printMotoInfo(moto.getPlate(), moto.getSpeed(), moto.getUid(), moto.getSoc());
+            moto = motoUpdate(moto, true);
         }
         for (segundos=0;segundos<12;segundos++) {
-            moto.setSoc(moto.calculaSoc(moto.getHost(), moto.calculaVel(false)));
-            timer++;
-            if (timer%10 == 0)
-                printMotoInfo(moto.getPlate(), moto.getSpeed(), moto.getUid(), moto.getSoc());
+            moto = motoUpdate(moto, false);
         }
     }
 
     // Rotina 3
     for (segundos=0;segundos<100;segundos++){
-        moto.setSoc(moto.calculaSoc(moto.getHost(), moto.calculaVel(true)));
-        timer++;
-        if (timer%10 == 0)
-            printMotoInfo(moto.getPlate(), moto.getSpeed(), moto.getUid(), moto.getSoc());
+        moto = motoUpdate(moto, true);
     }
 
     // Rotina 4
     for (segundos=0;segundos<32;segundos++){
-        moto.setSoc(moto.calculaSoc(moto.getHost(), moto.calculaVel(false)));
-        timer++;
-        if (timer%10 == 0)
-            printMotoInfo(moto.getPlate(), moto.getSpeed(), moto.getUid(), moto.getSoc());
+        moto = motoUpdate(moto, false);
     }
 
     return 0;
 }
 
-void printMotoInfo(string plate, float veloc, long long int batt, float carga){
-    cout << "Motorcycle plate: " << plate << endl;
-    cout << "Speed: " << veloc << endl;
-    cout << "Attached battery UID: " << batt << endl;
-    cout << "Motorcycle battery SoC: " << carga << "%" << endl;
+void printMotoInfo(Moto motoI){
+    cout << "Motorcycle plate: " << motoI.getPlate() << endl;
+    cout << "Speed: " << motoI.getSpeed() << endl;
+    cout << "Attached battery UID: " << motoI.getUid() << endl;
+    cout << "Motorcycle battery SoC: " << motoI.getSoc() << "%" << endl;
 }
 
 void printEtbInfo(int id, long long int uid, float soc, bool charg){
     cout << "CP " << id << ": " << uid << " | " << soc << "| charging: " << charg << endl;
+}
+
+Moto motoUpdate(Moto motoI, bool accel){
+    motoI.setSoc(motoI.calculaSoc(motoI.getHost(), motoI.calculaVel(accel)));
+    timer++;
+    if (timer%10 == 0)
+        printMotoInfo(motoI);
+    return motoI;
 }
